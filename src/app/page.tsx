47c2d159e9600d812303a36799d520ea746873c0
resenhandodirei <larimscorrea@gmail.com>
2025-04-07 15:00:24 -0300

    feat: componente de ResumoSobreMim feito

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import ResumoSobreMim from "./components/ResumoSobreMim";
 
 export default function Home() {
   return (
     <>
     <Navbar />
     <Hero />
-
+    <ResumoSobreMim />
     <Footer />
     </>
   );
